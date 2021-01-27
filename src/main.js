2f5c27189c5b1401ec75a7489995e834d61e727a
omer rokach <omerrokach@gmail.com>
2021-01-27 15:50:52 +0200

    geting all the relevent from the doc to the js

---
+++ src/main.js
@@ -0,0 +1,11 @@
+window.addEventListener("HTLonLoad", run)
+function run(){
+    let toDoList = document.getElementsByClassName("viewSection");
+    let select = document.getElementById("prioritySelector");
+    let input = document.getElementById("textInput");
+    let add = document.getElementById("addButton")
+    
+
+
+    
+}
