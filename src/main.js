a00a37baa691a037e947525e3e1b630d2eb8dee2
omer rokach <omerrokach@gmail.com>
2021-01-27 17:07:11 +0200

    fixing the append is not a func problem

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-window.addEventListener("HTLonLoad", run)
+window.addEventListener("DOMContentLoaded", run)
 function run(){
-    let toDoList = document.getElementsByClassName("viewSection");
-    let select = document.getElementById("prioritySelector");
-    let input = document.getElementById("textInput");
-    let add = document.getElementById("addButton")
-    
+    let counter = 0;
+    let viewSection = document.querySelector(".viewSection");
+    const select = document.getElementById("prioritySelector");
+    const input = document.getElementById("text-input");
+    const add = document.getElementById("add-button")
+    const taskArr = [];
+    add.addEventListener("click", e =>{
+        let inputVal = input.value;
+        let taskDate = new date();
+        // need 
+        taskArr.push(item);
+        input.value = '';
+        let task = document.createElement("div");
+        task.textContent = item;
+        viewSection.appendChild(task);
+    })
 
-
-    
 }
