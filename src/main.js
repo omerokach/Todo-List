6cd11fdc6bba121e1161b3e8d59e981710d75c13
omer rokach <omerrokach@gmail.com>
2021-01-28 17:40:34 +0200

    passin al the base tasts

--- src/main.js
+++ src/main.js
@@ -1,76 +1,126 @@
-window.addEventListener("DOMContentLoaded", run)
-function run(){
-    let count = 0;
-    let viewSection = document.querySelector(".viewSection");
-    const prioritySelector = document.getElementById("priority-selector");
-    const counter = document.getElementById("counter");
-    const input = document.getElementById("text-input");
-    const add = document.getElementById("add-button");
-    const sort = document.getElementById("sort-button");
-    const taskArr = [];
-    const priorityArr = []
-    add.addEventListener("click", e =>{
-        let taskObj = {inputVal:'' ,taskDate:'' ,taskPriority:'' ,};
-        let task = document.createElement("div");
-        task.classList.add("todo-container");
-        let taskText = document.createElement("span");
-        taskText.classList.add("todo-text");
-        let taskDate = document.createElement("span");
-        taskDate.classList.add("todo-created-at");
-        let taskPriority = document.createElement("span");
-        taskPriority.classList.add("todo-priority");
-        taskObj.inputVal = input.value;
-        taskText.textContent = ' ' + input.value;
-        taskObj.taskDate = clearDate (new Date());
-        taskDate.textContent = ' ' + clearDate (new Date());
-        taskObj.taskPriority = document.getElementById("priority-selector").selectedOptions[0].value;
-        taskPriority.textContent = ' ' + document.getElementById("priority-selector").selectedOptions[0].value;
-        task.append(taskPriority);
-        task.append(taskDate);
-        task.append(taskText);
-        taskArr.push(taskObj);
-        priorityArr.push(Number(taskObj.taskPriority));
-        viewSection.append(task);
-        input.value = '';
-        count ++;
-        counter.textContent = "The Number of Task's are "+ count;
-    })
-    sort.addEventListener("click", doSort)
-
-
-    function doSort(){
-        let viewSection = document.querySelector(".viewSection"); 
-        const taskDiv = document.querySelectorAll(".todo-container")
-        while (viewSection.hasChildNodes()) {  
-            viewSection.removeChild(viewSection.firstChild);
-          }
-        let newArr = [];
-        let prio = 1;
-        for(let task of taskArr){
-                newArr.push([task.taskPriority,task])
+// when refresh it take the info from the local storage
+window.addEventListener("DOMContentLoaded", e =>{
+    arrLocal = JSON.parse(localStorage.getItem("omer"));
+    printLocal(arrLocal);
+});
+//
+let arrLocal = [];
+let stringifiedTaskArr;
+let viewSection = document.querySelector(".viewSection");
+const prioritySelector = document.getElementById("priority-selector");
+const counter = document.getElementById("counter");
+const input = document.getElementById("text-input");
+const add = document.getElementById("add-button");
+const sort = document.getElementById("sort-button");
+const taskArr = [];
+let storageCounter;
+count = localStorage.getItem("counter");
+// adding the add event to add and sort button
+add.addEventListener("click", doAdd);
+sort.addEventListener("click", doSort)
+///================functions=================
+// the add event function ,add and updating the local storage the counter and tasks 
+function doAdd(){
+    let taskObj = {inputVal:'' ,taskDate:'' ,taskPriority:'' ,};
+    let container = document.createElement("div");
+    container.classList.add("todo-container");
+    let taskText = document.createElement("div");
+    taskText.classList.add("todo-text");
+    let taskDate = document.createElement("div");
+    taskDate.classList.add("todo-created-at");
+    let taskPriority = document.createElement("div");
+    taskPriority.classList.add("todo-priority");
+    taskObj.inputVal = input.value;
+    taskText.textContent = ' ' + input.value;
+    taskObj.taskDate = clearDate (new Date());
+    taskDate.textContent = ' ' + clearDate (new Date());
+    taskObj.taskPriority = document.getElementById("priority-selector").selectedOptions[0].value;
+    taskPriority.textContent = ' ' + document.getElementById("priority-selector").selectedOptions[0].value;
+    container.append(taskPriority);
+    container.append(taskDate);
+    container.append(taskText);
+    taskArr.push(taskObj);
+    viewSection.append(container);
+    input.value = '';
+    count ++;
+    storageCounter = count;
+    counter.textContent = "Number Of Task's: " + count;
+    stringifiedTaskArr = JSON.stringify(taskArr);
+    localStorage.setItem("omer", stringifiedTaskArr);
+    localStorage.setItem("counter", storageCounter)
+}
+function doSort(){
+    let viewSection = document.querySelector(".viewSection"); 
+    const taskDiv = document.querySelectorAll(".todo-container")
+    while (viewSection.hasChildNodes()) {  
+        viewSection.removeChild(viewSection.firstChild);
         }
-        newArr.sort(function(x, y) {
-            return x[0] - y[0];
-        });
-        for (let i=0; i<newArr.length; i++) {
-            let task = document.createElement("div");
-            task.classList.add("todo-container");
-            let taskText = document.createElement("span");
-            taskText.classList.add("todo-text");
-            let taskDate = document.createElement("span");
-            taskDate.classList.add("todo-created-at");
-            let taskPriority = document.createElement("span");
-            taskPriority.classList.add("todo-priority");
-            taskPriority.textContent = newArr[i][1].taskPriority;
-            taskDate.textContent = ' ' + newArr[i][1].taskDate;
-            taskText.textContent = ' ' + newArr[i][1].inputVal;
-            task.append(taskPriority);
-            task.append(taskDate);
-            task.append(taskText);
-            viewSection.appendChild(task);
-        }
+    let newArr = [];
+    let prio = 1;
+    for(let task of taskArr){
+            newArr.push([task.taskPriority,task]);
     }
-    function clearDate(date){
-        return `${date.getDate()}/0${date.getMonth()+1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`
+    newArr.sort(function(x, y) { return y[0] - x[0] ; });
+    for (let i=0; i<newArr.length; i++) {
+        let container = document.createElement("div");
+        container.classList.add("todo-container");
+        let taskText = document.createElement("div");
+        taskText.classList.add("todo-text");
+        let taskDate = document.createElement("div");
+        taskDate.classList.add("todo-created-at");
+        let taskPriority = document.createElement("div");
+        taskPriority.classList.add("todo-priority");
+        taskPriority.textContent = newArr[i][1].taskPriority;
+        taskDate.textContent = ' ' + newArr[i][1].taskDate;
+        taskText.textContent = ' ' + newArr[i][1].inputVal;
+        container.append(taskPriority);
+        container.append(taskDate);
+        container.append(taskText);
+        viewSection.appendChild(container);
+    }
+}
+// giving a nice and clear time and day
+function clearDate(date){
+    if(date.getHours() <10 && date.getMinutes() <10)
+    return  `${date.getDate()}/0${date.getMonth()+1}/${date.getFullYear()} 0${date.getHours()}:0${date.getMinutes()}`;
+    if(date.getHours()<10)
+    return  `${date.getDate()}/0${date.getMonth()+1}/${date.getFullYear()} 0${date.getHours()}:${date.getMinutes()}`;
+    if(date.getMinutes() < 10)
+    return  `${date.getDate()}/0${date.getMonth()+1}/${date.getFullYear()} ${date.getHours()}:0${date.getMinutes()}`;
+    return `${date.getDate()}/0${date.getMonth()+1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+}
+//adding item by using enter key
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("add-button").click();
+    }
+});
+//printing the tasks from the local storage
+function printLocal(arr){
+    if(arr === null) return;
+    if(arrLocal.length !==0 );
+    for(let i=0; i<arr.length; i++){
+    let taskObj = {inputVal:'' ,taskDate:'' ,taskPriority:'' ,};
+    let container = document.createElement("div");
+    container.classList.add("todo-container");
+    let taskText = document.createElement("div");
+    taskText.classList.add("todo-text");
+    let taskDate = document.createElement("div");
+    taskDate.classList.add("todo-created-at");
+    let taskPriority = document.createElement("div");
+    taskPriority.classList.add("todo-priority");
+    taskObj.inputVal = arr[i].inputVal;
+    taskText.textContent = ' ' + arr[i].inputVal;
+    taskObj.taskDate = arr[i].taskDate;
+    taskDate.textContent = ' ' + arr[i].taskDate;
+    taskObj.taskPriority = arr[i].taskPriority;
+    taskPriority.textContent = ' ' + arr[i].taskPriority;
+    container.append(taskPriority);
+    container.append(taskDate);
+    container.append(taskText);
+    taskArr.push(taskObj);
+    counter.textContent = "Number Of Task's: " + localStorage.getItem("counter");
+    viewSection.append(container);
     }
 }
